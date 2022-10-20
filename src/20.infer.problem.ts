c105ed95897f50038d64d81391e5597f84cda48b
mg <mag190033@gmail.com>
2022-10-21 00:51:10 +0200

    add infer

---
+++ src/20.infer.problem.ts
@@ -0,0 +1,13 @@
+// 1. figure out what is the type passed in to an Array<T>
+
+type WhatIsArrayType = any
+
+const arrayOfStrings = ['hello', 'world'];
+type Test = WhatIsArrayType<typeof arrayOfStrings>;
+const res: Test = 'hello world';
+
+const arrayOfNumbers = [1, 2];
+type Test1 = WhatIsArrayType<typeof arrayOfNumbers>;
+const res1: Test1 = 100;
+
+export {}
